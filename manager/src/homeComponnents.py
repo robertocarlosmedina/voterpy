a078c8f66b6858f6bb00c9d1f0166ca610b1ab9e
RobertoCarlosMedina <rmedina@uta.cv>
2021-04-13 13:46:23 -0100

    Starting the code of the manager Home.

---
+++ manager/src/homeComponnents.py
@@ -0,0 +1,21 @@
+import pygame
+from support.client import Client
+
+# Creating an object of the class client
+client = Client()
+inputBoxs = {"Name":["", False],"Age":["",False],"Descritpion":["",False],"Color":["",False]}
+
+def newCandidateRegistration(events, mouse_pos):
+    print("register")
+
+def viewCandidatesOnRegister(events, mouse_pos):
+    print("All register")
+
+def viewVotersOnRegister(events, mouse_pos):
+    print("All Voters")
+
+def countVotes(events, mouse_pos):
+    print("Count Votes")
+
+def pollInfo(events, mouse_pos):
+    print("Poll Info")
--- manager/src/homepage.py
+++ manager/src/homepage.py
@@ -1,5 +1,6 @@
 import pygame
 from support.color import Color
+from src.homeComponnents import *
 from support.buttons import verticalButtonsDisplay
 
 # class that draw the home screen
@@ -11,13 +12,15 @@ class HomePage:
         self.font2 = pygame.font.SysFont("arial", 25)
         self.surface = pygame.Surface((200,350))
         self.surface.fill(Color.grey3.value)
-        self.buttons = {"Register":1,"Candidates":2,"Voters":3,"Count Votes":1,\
-                        "Poll Info":20,}
-        self.active = 'Register'
+        self.buttons = {"Register":newCandidateRegistration,"Candidates":viewCandidatesOnRegister,\
+                        "Voters":viewVotersOnRegister,"Count Votes":countVotes,"Poll Info":pollInfo}
+        self.active = ''
         self.mouse_pos = None
+        self.events = None
 
     # Method the control this class
     def run(self, events):
+        self.events = events
         self.mouse_pos = pygame.mouse.get_pos()
 
         # Bliting on the screen title name
@@ -28,21 +31,32 @@ class HomePage:
         size = pygame.font.Font.size(self.font1, 'Manager-App')
         line = self.font1.render('Manager-App', True, Color.white.value)
         self.screen.blit(line, (self.screen_size[0]/2+225-size[0]/2, 70))
-        # Bliting the surface
+        # Bliting the surface and the line
         self.screen.blit(self.surface,(self.screen_size[0]/2+280-150,110))
+        pygame.draw.line(self.screen, Color.grey1.value, (40, 110),(420, 110), 2)
 
         # Calling method that draw the screen content according to the buttons
         self.drawbuttonContent()
 
         # Drwing buttons on the screen
-        self.active = verticalButtonsDisplay(self.screen, self.buttons.keys(),170,(481, 177),(198, 40), self.mouse_pos,self.active,\
+        self.active = verticalButtonsDisplay(self.screen, self.buttons.keys(),170,(481, 180),(198, 40), self.mouse_pos,self.active,\
              pygame.font.SysFont("arial", 20))
+        
         return "homePage"
     
     def drawbuttonContent(self):
+        # If there is no active function the display on the top function will be manager app
+        if self.active == "":
+            size = pygame.font.Font.size(self.font2, "Manager App")
+            line = self.font2.render("Manager App", True, Color.white.value)
+            self.screen.blit(line, (self.screen_size[0]/2-230-size[0]/2, 80))
+
+        # checking the key on the dict that is active and run the method tha is related to it 
         for key,value in self.buttons.items():
             if key == self.active:
                 size = pygame.font.Font.size(self.font2, key)
                 line = self.font2.render(key, True, Color.white.value)
-                self.screen.blit(line, (self.screen_size[0]/2-240-size[0]/2, 80))
-                pygame.draw.line(self.screen, Color.grey1.value, (40, 110),(420, 110), 2)
+                self.screen.blit(line, (self.screen_size[0]/2-230-size[0]/2, 80))
+                value(self.events, self.mouse_pos)
+
+                
